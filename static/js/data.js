279b19e8f996517165d90fc667b73e9b60146198
BrRoman <editeur@traditions-monastiques.com>
2023-12-24 11:25:41 +0100

    Data improved: hat, color, 1st week of Advent.

--- static/js/data.js
+++ static/js/data.js
@@ -1,6 +1,44 @@
 const days = {
   'adv_1_0': {
-    'header': 'First Sunday of Advent',
+    'hat': 'Hic incipit annus liturgicus.',
+    'color': 'violet',
+    'header': 'Dominica I Adventus.',
     'body': 'Ad Vigilias dicitur Resp. <i>Aspiciens</i>.',
+  },
+  'adv_1_1': {
+    'hat': '',
+    'color': 'violet',
+    'header': 'De ea.',
+    'body': '',
+  },
+  'adv_1_2': {
+    'hat': '',
+    'color': 'violet',
+    'header': 'De ea.',
+    'body': '',
+  },
+  'adv_1_3': {
+    'hat': '',
+    'color': 'violet',
+    'header': 'De ea.',
+    'body': '',
+  },
+  'adv_1_4': {
+    'hat': '',
+    'color': 'violet',
+    'header': 'De ea.',
+    'body': '',
+  },
+  'adv_1_5': {
+    'hat': '',
+    'color': 'violet',
+    'header': 'De ea.',
+    'body': '',
+  },
+  'adv_1_6': {
+    'hat': '',
+    'color': 'violet',
+    'header': 'De eo.',
+    'body': '',
   },
 };
