29db122ec41cdbd3579f14f7e9a41733b859cc76
BrRoman <editeur@traditions-monastiques.com>
2023-12-25 07:05:51 +0100

    Winner: first steps; CSS: colors, bootstrap++.

--- static/js/functions.js
+++ static/js/functions.js
@@ -20,15 +20,15 @@ function get_first_sunday_of_advent(christmas, christmas_weekday) {
 
 function element(day, weekday, hat, color, header, body) {
   return (
-    '<div class="element">'
-    + '<div class="hat">' + hat + '</div>'
-    + '<div class="head"  >'
+    '<div class="element d-flex flex-column w-50 mb-2">'
+    + '<div class="hat fst-italic ms-3">' + hat + '</div>'
+    + '<div class="head d-flex m-0">'
     + '<span class="fas fa-square ' + color + '"></span>'
-    + '<span class="day">' + day + '</span>'
-    + '<span class="weekday">' + weekday_human_readable(weekday) + '</span>'
-    + '<span class="header">' + header + '</span>'
+    + '<span class="day brown fw-bold ms-2">' + day + '</span>'
+    + '<span class="weekday brown fw-bold ms-1 text-nowrap">' + weekday_human_readable(weekday) + ' -</span>'
+    + '<span class="header text-justify ms-1">' + header + '</span>'
     + '</div>'
-    + '<div class="body">' + body + '</div>'
+    + '<div class="body text-justify">' + body + '</div>'
     + '</div>'
   );
 }
--- static/js/refresh.js
+++ static/js/refresh.js
@@ -7,33 +7,42 @@ function refresh_ordo(year) {
 
     // Title:
     $('#content').append(
-        '<div class="title"> Ordo ' + first_sunday_of_advent.getFullYear() + '-' + (first_sunday_of_advent.getFullYear() + 1) + '</div>'
+        '<div class="title text-center orange w-100"> Ordo ' + first_sunday_of_advent.getFullYear() + '-' + (first_sunday_of_advent.getFullYear() + 1) + '</div>'
     );
 
     // Year:
     $('#content').append(
-        '<div class="year">' + first_sunday_of_advent.getFullYear() + '</div>'
+        '<div class="year text-center fw-bold blue w-100 p-1">' + first_sunday_of_advent.getFullYear() + '</div>'
     );
 
     // Month:
     $('#content').append(
-        '<div class="month">' + month_human_readable(first_sunday_of_advent.getMonth()) + '</div>'
+        '<div class="month text-center fw-bold green w-100 mb-3 p-1">' + month_human_readable(first_sunday_of_advent.getMonth()) + '</div>'
     );
 
     // Advent:
     for (let i = 0; i < advent_duration; i++) {
         const date = new Date(first_sunday_of_advent.getTime() + (i * 24 * 3600 * 1000));
         const day = date.getDate();
+        let zero = '';
+        if (day < 10) {
+            zero = '0';
+        }
         const weekday = date.getDay();
-        const ref = 'adv_' + Math.ceil((i + 1) / 7) + '_' + (i % 7);
-        console.log(ref);
+        const month = date.getMonth() + 1;
+        const ref_tempo = 'adv_' + Math.ceil((i + 1) / 7) + '_' + (i % 7);
+        let winner = days_tempo[ref_tempo];
+        const ref_sancto = month + '_' + zero + day;
+        if (days_sancto[ref_sancto] && days_sancto[ref_sancto]['force'] > days_tempo[ref_tempo]['force']) {
+            winner = days_sancto[ref_sancto];
+        }
         $('#content').append(element(
             day,
             weekday,
-            days[ref]['hat'],
-            days[ref]['color'],
-            days[ref]['header'],
-            days[ref]['body'],
+            winner['hat'],
+            winner['color'],
+            winner['header'],
+            winner['body'],
         )
         );
     }
