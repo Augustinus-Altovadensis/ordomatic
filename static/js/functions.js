aaf57a172803b5699549eb53804819ff6e63b4fa
BrRoman <editeur@traditions-monastiques.com>
2023-12-26 09:44:37 +0100

    Hat: CSS improved.

--- static/js/functions.js
+++ static/js/functions.js
@@ -37,7 +37,7 @@ function get_winner(ref_tempo, ref_sancto) {
 function element(day, weekday, hat, color, header, body) {
   return (
     '<div class="element d-flex flex-column w-50 mb-2">'
-    + '<div class="hat fst-italic ms-3">' + hat + '</div>'
+    + '<div class="hat fst-italic fw-bold text-center">' + hat + '</div>'
     + '<div class="head d-flex m-0">'
     + '<span class="fas fa-square ' + color + '"></span>'
     + '<span class="day brown fw-bold ms-2">' + day + '</span>'
