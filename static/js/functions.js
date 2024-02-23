03b0dbe6c9e5402fb02d541a0266cd22e39623cd
BrRoman <editeur@traditions-monastiques.com>
2024-02-23 10:12:21 +0100

    Test for new month simplified.

--- static/js/functions.js
+++ static/js/functions.js
@@ -101,13 +101,13 @@ function component(date, year, month, day, weekday, before, color, header, body,
   if (date.getFullYear() != year) {
     year = date.getFullYear();
     block_new_year = '<div class="year blue mt-5">' + year + '</div>';
-    if (date.getMonth() != month || day == 1) {
+    if (day == 1) {
       month = date.getMonth();
       block_new_month = '<div class="month green mb-3">Ianuarius</div>';
     }
   } else {
     block_new_year = '';
-    if (date.getMonth() != month || day == 1) {
+    if (day == 1) {
       month = date.getMonth();
       block_new_month = '<div class="month green my-3">' + month_human_readable(month) + '</div>';
     } else {
