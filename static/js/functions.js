46807092258936d78445d6b2b8cd754a95efa2fb
Augustinus-Altovadensis <jirka.emmer@volny.cz>
2026-08-07 17:18:25 +0200

    Removed "sine Com." from data files + further parts of old Comm. routine

--- static/js/functions.js
+++ static/js/functions.js
@@ -680,6 +680,18 @@ function period(duration, start, prefix_tempo, week_start, day_start, extra) {
         commemoratio_next = "";
       }
 
+    ////  Removing moved feasts from their original day  \\\\
+    ////=================================================\\\\\
+    if (moved.includes(ref_sancto))
+    {
+      if (commemoratio) 
+        {
+          winner = commemoratio;
+          commemoratio = "";
+        }
+      else winner = feria;
+    }
+
     //==========================
     // Debugging of moved feasts
     moved_beginning = '';
@@ -1733,81 +1745,13 @@ function period(duration, start, prefix_tempo, week_start, day_start, extra) {
 
       if ((commemoratio['laudes'] || commemoratio['laudes_commemoratio'] || comm_laudes) && !no_comm_laudes)
         { 
-          comm_laudes = comm_laudes.replace(/- sine Com\.|sine Com\./, "");
+          comm = ""
           laudes = laudes.replace(/- sine Com\.|sine Com\./, "");
-          comm_laudes = comm_laudes.replace("Com\. ", "");
-          comm = commemoratio['laudes_commemoratio'];
-          comm = comm.replace("Com\. ", "");
-          comm = ( comm == "C1") ? "Dum stetéritis. " : comm;
-          comm = ( comm == "C1a") ? "Isti sunt duæ olívæ. " : comm;
-          comm = ( comm == "C2") ? "Qui vult. " : comm;
-          comm = ( comm == "C2a") ? "Qui odit. " : comm;
-          comm = ( comm == "C3") ? "Fulgébunt justi. " : comm;
-          comm = ( comm == "C4") ? "Euge, serve bone. " : comm;
-          comm = ( comm == "C5") ? "Similábo eum. " : comm;
-          comm = ( comm == "C6") ? "Símile est ... sagénæ. " : comm;
-          comm = ( comm == "C7") ? "Símile est ... sagénæ. " : comm; // not an error
-          comm = ( comm == "C8") ? "Zachæe. " : comm;
-
-          // Glossary:
-          // comm_laudes = commemoratio['laudes'];
-          // comm = commemoratio['laudes_commemoratio'];
-          // titulum = commemoratio['header']
 
           // Advent changes on certain dates, only if in Commemoratio
           if (winner == days_sancto[ref_sancto] && month_usual_number == 12 && day >= 17 && day <=23 ) {
             if (day == 21) comm = comm.replace(/Adv\. <i>.*<\/i>/, "Adv. <i>Nolíte timére.</i>")
             if (day == 23) comm = comm.replace(/Adv\. <i>.*<\/i>/, "Adv. <i>Ecce compléta sunt.</i>") }
-
-          // In S. Familia, the Comm. of Oct. Epiph. is different...
-          if (winner == days_tempo['pe_1_0']) {
-              comm = comm.replace(/Oct\. Epiph\. Domini\. <i>Magi venérunt\.<\/i> <red>sine<\/red> <i>Alleluia\.<\/i>( \& )?/, "")
-              comm_laudes = ""
-            }
-
-          et = " & ";
-          dash = " – ";
-          if ( !titulum ) et = "";
-          if ( !comm_laudes ) et = "";
-          if ( !laudes ) dash = "";
-
-          if (laudes.match(/Feria/i) && ref_tempo.match(/lent_|ash_/))
-             {
-              laudes = laudes.replace(/Feria\.?/i, comm);
-              comm = "";
-             }
-          else if (laudes.match(/^Com\. | - Com\./) && comm) 
-            { // we need to sort the commemorations according to their force
-             if (commemoratio['force'] >= 30 ) {
-              if (!laudes.match(/Tu es pastor|Oct. Epiph.|Fer\./i)) laudes = laudes.replace("Com.", "Com. " + comm + " & ");
-              else laudes += " & " + comm;}
-              comm = "";}
-          else {
-          if ( commemoratio['laudes_commemoratio'].match(/^Com\. /) && comm )
-            { 
-              if (laudes.match(/^Com\. | - Com\./)) laudes = laudes + " & " + comm;
-              else if (laudes) laudes += " - Com. " + comm;
-              else laudes = "Com. " + comm;
-            }
-          else if ( commemoratio['laudes_commemoratio'].length > 3 && comm ) { 
-              laudes = laudes + dash + "Com. " + titulum + et + comm;}
-          else if ( commemoratio['laudes_commemoratio'] && commemoratio['laudes_commemoratio'].length <= 3 && comm ) {
-            if ( !comm_laudes ) et = "";
-            laudes = laudes + dash + "Com. " + titulum + " " + comm + et + comm_laudes; }
-          else if ( laudes.match(/^Com\. | - Com\./) ) laudes = laudes + et + comm_laudes;
-          else if ( commemoratio['laudes'].match("Com. ") ) laudes = laudes + dash + "Com. " + comm_laudes;
-          else if (comm) laudes = laudes + dash + "Com. " + comm;
-          }
-
-          if (commemoratio_add) {
-              if (commemoratio_add['laudes_commemoratio']) laudes += " & " + commemoratio_add['laudes_commemoratio'].replace(/Com\. /i,"");
-              else if (commemoratio_add['laudes']) laudes += " & " + commemoratio_add['laudes'];
-            }
-
-          // In S. Familia, the Comm. of Oct. Epiph. is different...
-          //if (winner == days_tempo['pe_1_0']) laudes = laudes.replace(/ ?\&? ?Oct\. Epiph\. Domini\. <i>Magi venérunt\.<\/i> <red>sine<\/red> <i>Alleluia\.<\/i> ?\&? ?/, "")
-
-          //BACKUP else laudes = laudes + dash + "Com. " + titulum + " " + comm + et + comm_laudes;
 
           if ( winner['force'] > 49 ) { laudes.replace("& B.M.V.", "");}
           comm = null;
@@ -1822,163 +1766,21 @@ function period(duration, start, prefix_tempo, week_start, day_start, extra) {
       // On Pent. Vigil, a 3-Lesson feast may fall, cannot however be commemorated in Mass.
         { 
           if (!comm_missa) comm_missa = commemoratio['missa'];
-          
-          // Replacing the word "Feria" in [missa] field to keep other commemorations, while only the Feria is commemorated (like at feasts of St. Peter and Paul in Lent)
-          if (missa.match(/Feria/i) && !ref_tempo.match(/lent_|ash_/) )
-              missa = missa.replace(/.a Feria\.? -/i, "-" );
-          else if (missa_post.match(/Feria/i) && !ref_tempo.match(/lent_|ash_/) )
-              missa_post = missa_post.replace(/.a Feria\.? -/i, "-" );
-          //else if (missa.match(/Feria/i) && ref_tempo.match(/lent|adv/) && commemoratio == days_tempo[ref_tempo])
-          else if (missa.match(/Feria/i) && ref_tempo.match(/lent_|ash_/))
-              missa = missa.replace(/Feria/i, translate_feria(ref_tempo, 1));
-          else if (missa_post.match(/Feria/i) && commemoratio == days_tempo[ref_tempo])
-              missa_post = missa_post.replace(/Feria/i, translate_feria(ref_tempo, 1));
-
-          else if (missa.match(/Dominica/i) && ref_tempo.match(/adv/i)) 
-              missa = missa.replace(/Dominica/ig, "Dom. " + roman_lc[ref_tempo.substring(4,5)] + " Adv.");
-
-          // Sorting out Commemoratio -vel-
-          else if (missa.match(/Commemoratio -vel-/i) ) {
-              secunda_comm = missa.match(/-vel-.*? -/i) + "";
-              if (missa.match("3a non dicitur")) missa = missa.replace(/Commemoratio -vel-.*? -/i, titulum_missa + " <u>3a non dicitur</u> - ");
-              else if (missa.match(/3a Commemoratio -vel-/i) ) missa = missa.replace(/Commemoratio -vel-.*? -/i, titulum_missa + " - "); 
-              else missa = missa.replace(/Commemoratio -vel-.*? -/i, titulum_missa + " 3a " + secunda_comm.replace(/-vel- /i, "")); }
-          else if (missa_post.match(/Commemoratio -vel-/i) ) {
-              secunda_comm = missa_post.match(/-vel-.*? -/i) + "";
-              if (missa_post.match("3a non dicitur")) missa_post = missa_post.replace(/Commemoratio -vel-.*? -/i, titulum_missa + " <u>3a non dicitur</u> - ");
-              else if (missa_post.match(/3a Commemoratio -vel-/i) ) missa_post = missa_post.replace(/Commemoratio -vel-.*? -/i, titulum_missa + " - ");
-              else missa_post = missa_post.replace(/Commemoratio -vel-.*? -/i, titulum_missa + " 3a " + secunda_comm.replace(/-vel- /i, "")); }
-
-          // Sorting out Commemoratio et M., so the Comm. work as supposed, while Missa is taken from the Comm.
-          else if (commemoratio['rank'].match(/Commemoratio et M/i) && winner['force'] == 10) missa = commemoratio['missa'];
-            
-          else 
-          {
-          comm_missa = comm_missa.match(/2a.*? -/) + ""; // looks stupid, but converts the variable into a string that the replace function can take
-
-          // if we commemorate something, apart from lent, we don't commemorate the Ferias
-          if (!ref_tempo.match(/(lent|ash)/)) {
-            if (comm_missa.match(/de feria/i)) comm_missa = comm_missa.replace(/a de feria\. 3/i,""); 
-            if (comm_missa.match(/de feria/i)) comm_missa = comm_missa.replace(/.a de feria\./i,""); 
-            if (comm_missa.match(/de (Off\.|Officio) diei/i)) comm_missa = comm_missa.replace(/a de (Off\.|Officio) diei\. 3/i,""); 
-            if (comm_missa.match(/de (Off\.|Officio) diei/i)) comm_missa = comm_missa.replace(/.a de (Off\.|Officio) diei\./i,""); }
-
-          // On Sunday, Com. de Feria needs to be deleted: "de Feria. 3a "
-          if (weekday == 0 && comm_missa.match(/de feria/i)) comm_missa = comm_missa.replace(/a de feria\. 3/i,""); 
-
-          // Needs to be tested. Originally the following line said:
-          // if (!(commemoratio['force'] == 9 && commemoratio['missa'].match(/2a S\./)) (this condition was negated)
-          if (!(commemoratio['force'] == 9 && commemoratio['missa'].match(/2a S\./)) && (winner != days_sancto['votiva_bmv'] || winner != days_sancto['votiva_bmv_prima_sabb'])) {
-            comm_missa = comm_missa.replace(/3a.*/,""); 
-            comm_missa = comm_missa.replace("2a", "3a"); 
-            if (ref_tempo.match("adv_") && winner == days_sancto[ref_sancto]) de = "de "; else de = "";
-            if (comm_missa.length > 5) comm_missa = "2a " + de + titulum_missa + ". " + comm_missa; else comm_missa = "2a " + titulum_missa + ". ";
-            }
-
-          if (commemoratio['force'] == 9 && commemoratio['missa'].match(/2a S\./) && (winner == days_sancto['votiva_bernardi'] || winner == days_sancto['xxx'])) {
-            comm_missa = comm_missa.replace(/3a.*/,""); 
-            comm_missa = comm_missa.replace("2a", "3a"); 
-            if (ref_tempo.match("adv_") && winner == days_sancto[ref_sancto]) de = "de "; else de = "";
-            if (comm_missa.length > 5) comm_missa = "2a " + de + titulum_missa + ". " + comm_missa; else comm_missa = "2a " + titulum_missa + ". ";
-            }
-
-          comm_missa = comm_missa.replace(/-.*/, ""); 
-
-          // In MM. maj. and higher, we get rid of 3a de S. Maria (and later all variants)
-          if (winner['force'] >= 80) {
-              comm_missa = comm_missa.replace(/.a (?:de S\. Maria |B\. ?M\. ?V\. ? )?A cunctis\.? ?/, "")
-              comm_missa = comm_missa.replace(/.a (?:de S\. Maria)? Conc[ée]de nos\.? ?/, "")
-            }
-
-          /////  Vigilia Assumptionis, 3a oratio in missa \\\\\
-          if (winner == days_sancto['08_14v']) {
-            comm_missa = comm_missa.replace(/A cunctis/i, "de Sp. Sancto") }
-
-          ///// In Corpus Christi Octave, A cunctis. needs to be replaced with Concéde nos.
-          if (ref_tempo.match(/pa_1_[56]|pa_2_[123]/) && comm_missa && comm_missa.match(/A cunctis/i))
-              comm_missa = comm_missa.replace(/A cunctis/i, "Concéde nos")
-
-          var comm_temp = "";
-          if (true && (commemoratio['missa'].match(/4a /i) || (commemoratio['missa'].match(/3a S\./i) && commemoratio['force'] != 9) )) {
-            comm_temp = commemoratio['missa'].match(/3a.*? -/) + "";
-            comm_temp = comm_temp.replace(" -", ""); 
-            comm_temp = comm_temp.replace("4a", "5a"); 
-            comm_temp = comm_temp.replace("3a", "4a"); 
-            comm_temp = comm_temp.replace("2a", "3a"); 
-            }
-
-          if (comm_temp) comm_missa += " " + comm_temp;
-
-          // On Votiva S.P.N.Bernardi, and everywhere, where there is no comm. in winner['missa'], multiple comm. were rendered incorrectly...
-          if (false && winner == days_sancto['votiva_bernardi']) {
-            comm_missa = commemoratio['missa'].match(/2a.*? -/) + ""; 
-            comm_missa = comm_missa.replace(" -", ""); }
-          // probably not needed anymore
-          
-          win_missa = missa;
-          if (win_missa.match("2a") && winner['force'] > 40 && win_missa.match(/2a S\. |2a Fer\. |2a (?:de )?Dom/i)) 
-            { 
-            win_missa_com = win_missa.match(/2a.*? -/) + "";
-            win_missa_com = win_missa_com.replace(" -", "") + "";
-            } else win_missa_com = "";
-          if (win_missa_com) {
-            comm_missa = comm_missa.replace(/3a.*/,""); 
-            comm_missa = comm_missa.replace("2a", "3a"); 
-            comm_missa = win_missa_com + " " + comm_missa; }
-
-          win_missa = win_missa.replace(/2a.*? - /,"");
-          win_missa_post = winner['missa_post'];
-          win_missa_post = win_missa_post.replace(/2a.*? - /,""); // non-greedy modifier "?"
-
-          // Sometimes, a lower feast is comm. on Sunday, and we need to add "3a de Beata"
-          if (commemoratio['force'] >= 40 && winner['force'] < 70 && !winner['header'].match(/B\.M\.V\.|B\. M\. V\./) && !comm_missa.match("3a")) comm_missa += " 3a A cunctis. ";
-
-          // We have to check, whether the missa field exists.
-          // Sometimes, in order to add exceptions, only missa_post exists
-          // with differences between missa privata and conventualis
-          if (winner['missa']) {
-            if (winner['missa'].match("Glo.</b>"))
-              missa = win_missa.replace("Glo.</b>", "Glo.</b> – " + comm_missa);
-            else if (winner['missa'].match("Glo."))
-              missa = win_missa.replace("Glo.", "Glo. – " + comm_missa);
-            //else missa = comm_missa + " - " + win_missa; }
-            else missa = missa.replace(/2.*? - /, comm_missa + " - "); }
-          else if (winner['missa_post']) {
-            if (winner['missa_post'].match("Glo."))
-              missa_post = win_missa_post.replace("Glo.", "Glo. – " + comm_missa);
-            else missa_post = comm_missa + " - " + win_missa_post; }
-          }
-
-          if (commemoratio_add && commemoratio_add['header']) {
-            titulum_add = commemoratio_add['header'].split(",", 1);
-            titulum_add = " " + titulum_add;
-            if (commemoratio_add['missa']) 
-              {
-                next_ora = "3a";
-                //missa = missa.replace(/2a (.*?) -/i, "2a " + "$1" + next_ora + titulum_add + ". -");
-                missa = missa.replace(/.a de S\. Maria(.*?)\./i, "");
-                missa = missa.replace(/2a (.*?) -/i, "2a " + "$1" + next_ora + titulum_add + ". -");
-              }
-            }
 
           // If Sunday yields to another Feast with Comm., it needs to be added.
-          if (weekday == 0 && (commemoratio == days_tempo[ref_tempo] 
+          if (weekday == 0 && !moved.includes(ref_sancto)
+            && (commemoratio == days_tempo[ref_tempo] 
               || winner == days_sancto['nomen_jesu'])) {
             if (!missa.match("Asperges")) missa = "Asperges - " + missa;
             missa = missa.replace(/Duo Acolythi\.?(?: -)?/, "");
             missa = missa.replace(/Cum incenso ad oblata\.?(?: - )?/i, "");
-            if (!missa.match("Processio") && ref_tempo.match(/tp_|pa_/) && month_usual_number <= 9) //missa = 'Processio per Ecclesiam - ' + missa; // orig. Claustrum
+            if (!missa.match("Processio") && ref_tempo.match(/tp_|pa_/) && month_usual_number <= 9) 
+            //missa = 'Processio per Ecclesiam - ' + missa; // orig. Claustrum
                missa = missa.replace("Asperges", "Asperges - Processio per Claustrum")
-            if (!missa.match("Sub tuum") && ref_tempo.match(/pe_|pa_/) && month_usual_number > 9) missa = missa.replace("Asperges", "Asperges - Sub tuum")
+            if (!missa.match("Sub tuum") && ref_tempo.match(/pe_|pa_/) && month_usual_number > 9) 
+              missa = missa.replace("Asperges", "Asperges - Sub tuum")
             if (!missa.match("Cre.")) missa = missa.replace(/Pr(ae|æ)f\./i, "Cre. - Præf.")
             if (!missa.match(/In fine Miss.*Evang/i)) missa += ' - <red>In fine Missæ Evangelium Dominicæ.</red>';
-            if (winner['laudes'].match("Com.") && !winner['laudes'].match("sine Com."))
-              { 
-                tertia_oratio = winner['missa'].match(/2a.*? -/i);
-                tertia_oratio += "";
-                tertia_oratio = tertia_oratio.replace("2a", "3a");
-                missa = missa.replace(/2a.*? -/i, "2a " + titulum_missa + " " + tertia_oratio)
-              }
             laudes_post = "<li>- <red>non dicitur </red><i>Quicúmque.</i></li>" + laudes_post;
             }
 
@@ -1987,9 +1789,6 @@ function period(duration, start, prefix_tempo, week_start, day_start, extra) {
             dominica = ref_tempo.slice(0, -1) + '0';
             missa = missa.replace(/-De Dominica-/i, days_tempo[dominica]['vesperae']);
             }
-
-          // In MM. maj. and higher, we get rid of 3a de S. Maria (and later all variants)
-          if (winner['force'] > 70) missa = missa.replace(/3a De S\. Maria\. |3a B\..?M\..?V\. Concéde nos\.?/i, "");
 
           // Cleanup:
           missa = missa.replaceAll("  ", " "); missa = missa.replace("..", ".");
@@ -2021,26 +1820,6 @@ function period(duration, start, prefix_tempo, week_start, day_start, extra) {
 
           vesperae = vesperae.replace(/- sine Com\.|sine Com\./, "");
           comm_vesperae = comm_vesperae.replace(/- sine Com.|sine Com./, "");
-
-          comm = commemoratio['vesperae_commemoratio'];
-          comm = ( comm == "C1") ? "Ecce ego mitto vos. " : comm;
-          comm = ( comm == "C12") ? "Beáti eritis. " : comm;
-          comm = ( comm == "C2") ? "Beátur vir. " : comm;
-          comm = ( comm == "C2a") ? "Iste Sanctus. " : comm;
-          comm = ( comm == "C22") ? "Hic est vere Martyr. " : comm;
-          comm = ( comm == "C3") ? "Isti sunt Sancti. " : comm;
-          comm = ( comm == "C32") ? "Tradidérunt. " : comm;
-          comm = ( comm == "C4") ? "Sacérdos et Póntifex. " : comm;
-          comm = ( comm == "C42") ? "Amávit eum Dóminus. " : comm;
-          comm = ( comm == "C42a") ? "Dum esset summus Póntifex. " : comm;
-          comm = ( comm == "C4d") ? "O Doctor óptime. " : comm;
-          comm = ( comm == "C5") ? "Iste cognóvit. " : comm;
-          comm = ( comm == "C52") ? "Iste Sanctus. " : comm;
-          comm = ( comm == "C6") ? "Veni, sponsa Christi. " : comm;
-          comm = ( comm == "C62") ? "Quinque prudéntes Vírgines. " : comm;
-          comm = ( comm == "C7") ? "Símile est... hómini. " : comm;
-          comm = ( comm == "C8") ? "Pax ætérna. " : comm;
-          comm = ( comm == "C82") ? "O quam metuéndus. " : comm;
 
         if (winner == days_sancto['votiva_bernardi']
          || winner == days_sancto['votiva_sacramentum']) comm = "";
@@ -2282,6 +2061,13 @@ function period(duration, start, prefix_tempo, week_start, day_start, extra) {
                 comm_missa_add.push({force: 2, header: 'de S. Maria Concéde nos.'});
                 comm_missa_add.push({force: 1, header: 'Ecclésiæ. vel pro Papa.'});
               }
+            else if (ref_tempo.match(/pa_1_[56]|pa_2_[123]/))
+              {
+                // Octave SS. Corporis Christi doesn't have A cunctis
+                comm_missa_add.push({force: 2, header: 'de S. Maria Concéde nos.'});
+                comm_missa_add.push({force: 1, header: 'Ecclésiæ. vel pro Papa.'});
+              }
+
             //comm_missa_add.sort((a, b) => b.force - a.force);
             if (winner == days_sancto['votiva_bmv']
              || winner == days_sancto['votiva_bmv_prima_sabb']
